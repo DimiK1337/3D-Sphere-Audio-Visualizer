6ac2a7b96590f61608acf9d859eb7e5adac1a24c
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-01-25 23:03:27 +0100

    VITE.CONFIG.JS: Added local var to reference custom backend for frontend to make requests to

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 
+
+const backendUrl = "http://localhost:3000";//process.env.VITE_BACKEND_URL;
+
 export default defineConfig({
     server: {
         proxy: {
             '/download-audio': {
-                target: process.env.VITE_BACKEND_URL,
+                target: backendUrl,
                 changeOrigin: true,
             },
             '/audio': {
-                target: process.env.VITE_BACKEND_URL,
+                target: backendUrl,
                 changeOrigin: true,
             },
         },
